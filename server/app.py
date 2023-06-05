8dd61ec9bb045a27d65fa11cce6f2b5f720a6be5
Chris Sampson <csamposn@gmail.com>
2023-06-05 14:21:22 -0400

    Added SQL ALchemy

--- server/app.py
+++ server/app.py
@@ -1,9 +1,20 @@
+from os import environ
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # test routes
 from routers.testRoutes import serverStatusBlueprint
 
 app = Flask(__name__)
+
+db = SQLAlchemy()
+
+app.config["SQLALCHEMY_DATABASE_URI"] ='postgresql://postgres:postgres@localhost:5432/test'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 # server status
 app.register_blueprint(serverStatusBlueprint, url_prefix='/test/status')
